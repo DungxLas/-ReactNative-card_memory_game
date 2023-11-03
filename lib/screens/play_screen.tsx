7cab1d555143bcd333890ff5b1de9502bd3b44ac
DungxLas <phamjhungfdungx@gmail.com>
2023-11-03 14:57:07 +0700

    03/11/2023

--- lib/screens/play_screen.tsx
+++ lib/screens/play_screen.tsx
@@ -1,8 +1,28 @@
-import { Text, View } from "react-native";
+import {Dimensions, FlatList, Image} from 'react-native';
+
+const images = [
+  {source: require('./assets/image/bird.png')},
+  {source: require('./assets/image/cat.png')},
+  {source: require('./assets/image/dog.png')},
+  {source: require('./assets/image/lizard.png')},
+  {source: require('./assets/image/mouse.png')},
+  {source: require('.assets/image/rabbit.png')},
+];
+
+const size = Dimensions.get('window').width / 3;
 
 const PlayScreen = () => {
-    return <View>
-        <Text>edhfnsdjfisdj</Text>
-    </View>
-}
+  return (
+    <FlatList
+      data={images}
+      renderItem={({item}) => {
+        return (
+          <Image source={item.source} style={{width: size, height: size}} />
+        );
+      }}
+      keyExtractor={(_, index) => index.toString()}
+      numColumns={2}
+    />
+  );
+};
 export default PlayScreen;
